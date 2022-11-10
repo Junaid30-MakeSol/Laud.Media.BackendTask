2c82e9b9ac70347bf86b42e0f1e96677fbcac71c
Junaid Butt <junaid@dbate.no>
2022-11-10 16:43:41 +0100

    Added currency interface in business logic application library

---
+++ Laud.Media.Application/Interfaces/ICurrencyManager.cs
@@ -0,0 +1,14 @@
+﻿using Laud.Media.Application.Models;
+using Laud.Media.Application.Models.Currency;
+
+namespace Laud.Media.Application.Interfaces
+{
+    public interface ICurrencyManager
+    {
+        List<CurrencyModel> GetAllCurrencies();
+        CurrencyModel GetCurrencyById(int id);
+        void Create(CurrencyModel model);
+        ResultModel Update(CurrencyModel model);
+        void Delete(int id);
+    }
+}
